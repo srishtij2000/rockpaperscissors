8e9e44124fc7f3d1b18e67616c8cbc2994a71137
srishtij2000 <64734684+srishtij2000@users.noreply.github.com>
2020-05-31 17:13:06 +0530

    rockpaperscissors

---
+++ text12.js
@@ -0,0 +1,112 @@
+var user_score = 0;
+var comp_score = 0;
+var user_span = document.getElementById("userscore");
+var comp_span = document.getElementById("compscore");
+var result_p = document.getElementById("result");
+var rock_img = document.getElementById("rock");
+var paper_img = document.getElementById("paper");
+var scissors_img = document.getElementById("scissors");
+var scoreboard = document.querySelector(".scoreboard");
+var button = document.getElementById("button");
+function getcomputerchoice() {
+  var choices = ["r", "p", "s"];
+  var random = Math.floor(Math.random() * 3);
+  return choices[random];
+}
+function convert(letter) {
+  if (letter == "r") return "Rock";
+  if (letter == "p") return "Paper";
+  if (letter == "s") return "Scissors";
+}
+function wins(userchoice, computerchoice) {
+  user_score++;
+  user_span.innerHTML = user_score;
+  comp_span.innerHTML = comp_score;
+  var small = "user".fontsize(3).sup();
+  var big = "computer".fontsize(3).sup();
+  result_p.innerHTML =
+    convert(userchoice) +
+    small +
+    " beats " +
+    convert(computerchoice) +
+    big +
+    " . You win! ";
+  document.getElementById(userchoice).classList.add("green-glow");
+  setTimeout(function () {
+    document.getElementById(userchoice).classList.remove("green-glow");
+  }, 1000);
+}
+function lose(userchoice, computerchoice) {
+  comp_score++;
+  user_span.innerHTML = user_score;
+  comp_span.innerHTML = comp_score;
+  var small = "user".fontsize(3).sup();
+  var big = "computer".fontsize(3).sup();
+  result_p.innerHTML =
+    convert(computerchoice) +
+    big +
+    " beats " +
+    convert(userchoice) +
+    small +
+    " . You lose !";
+  document.getElementById(userchoice).classList.add("red-glow");
+  setTimeout(function () {
+    document.getElementById(userchoice).classList.remove("red-glow");
+  }, 1000);
+}
+function draw(userchoice, computerchoice) {
+  user_span.innerHTML = user_score;
+  comp_span.innerHTML = comp_score;
+  var small = "user".fontsize(3).sup();
+  var big = "computer".fontsize(3).sup();
+  result_p.innerHTML =
+    convert(userchoice) +
+    small +
+    " draw " +
+    convert(computerchoice) +
+    big +
+    " . It's a tie ! ";
+  document.getElementById(userchoice).classList.add("grey-glow");
+  setTimeout(function () {
+    document.getElementById(userchoice).classList.remove("grey-glow");
+  }, 1000);
+}
+function game(userchoice) {
+  var computerchoice = getcomputerchoice();
+  if (userchoice == computerchoice) {
+    draw(userchoice, computerchoice);
+  } else if (userchoice == "r" && computerchoice == "s") {
+    wins(userchoice, computerchoice);
+  } else if (userchoice == "p" && computerchoice == "r") {
+    wins(userchoice, computerchoice);
+  } else if (userchoice == "s" && computerchoice == "p") {
+    wins(userchoice, computerchoice);
+  } else if (userchoice == "r" && computerchoice == "s") {
+    lose(userchoice, computerchoice);
+  } else if (userchoice == "p" && computerchoice == "r") {
+    lose(userchoice, computerchoice);
+  } else if (userchoice == "s" && computerchoice == "r") {
+    lose(userchoice, computerchoice);
+  }
+}
+function reset() {
+  user_score = 0;
+  comp_score = 0;
+  user_span.innerHTML = 0;
+  comp_span.innerHTML = 0;
+}
+function main() {
+  rock_img.addEventListener("click", function () {
+    game("r");
+  });
+  paper_img.addEventListener("click", function () {
+    game("p");
+  });
+  scissors_img.addEventListener("click", function () {
+    game("s");
+  });
+  button.addEventListener("click", function () {
+    reset();
+  });
+}
+main();
